da8eea3a96254240d1abbb22a04451704ead183b
Tiaguinhooo <tiago.andre.soares.ferreira2008@gmail.com>
2025-12-01 21:29:52 +0000

    Second commit

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
 const express = require('express');
+const mysql = require('mysql2');
+
+require('dotenv').config();
+
+const app = express();
+const port = 3000;
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+app.get('/', (req, res) => {
+    res.send("Teste");
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+})
